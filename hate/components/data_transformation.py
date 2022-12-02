b9cbee177abb347fdad93bb330fe603e30f7de83
jaydeepIneuron007 <jaydeep@ineuron.ai>
2022-12-02 18:24:22 +0530

    data transformation added

---
+++ hate/components/data_transformation.py
@@ -0,0 +1,110 @@
+import os
+import re
+import sys
+import string
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+from sklearn.model_selection import train_test_split
+from hate.logger import logging 
+from hate.exception import CustomException
+from hate.entity.config_entity import DataTransformationConfig
+from hate.entity.artifact_entity import DataIngestionArtifacts, DataTransformationArtifacts
+
+class DataTransformation:
+    def __init__(self,data_transformation_config: DataTransformationConfig,data_ingestion_artifacts:DataIngestionArtifacts):
+        self.data_transformation_config = data_transformation_config
+        self.data_ingestion_artifacts = data_ingestion_artifacts
+
+    def imbalance_data_cleaning(self):
+
+        try:
+            imbalance_data=pd.read_csv(self.data_ingestion_artifacts.imbalance_data_file_path)
+            imbalance_data.drop(self.data_transformation_config.ID,axis=self.data_transformation_config.AXIS , 
+            inplace = self.data_transformation_config.INPLACE)
+
+            return imbalance_data 
+        except Exception as e:
+            raise CustomException(e,sys) from e 
+
+    def raw_data_cleaning(self):
+        
+        try:
+            raw_data = pd.read_csv(self.data_ingestion_artifacts.raw_data_file_path)
+            raw_data.drop(self.data_transformation_config.DROP_COLUMNS,axis = self.data_transformation_config.AXIS,
+            inplace = self.data_transformation_config.INPLACE)
+
+            raw_data[raw_data[self.data_transformation_config.CLASS]==0][self.data_transformation_config.CLASS]=1
+            
+            # replace the value of 0 to 1
+            raw_data[self.data_transformation_config.CLASS].replace({0:1},inplace=True)
+
+            # Let's replace the value of 2 to 0.
+            raw_data[self.data_transformation_config.CLASS].replace({2:0}, inplace = True)
+
+            # Let's change the name of the 'class' to label
+            raw_data.rename(columns={self.data_transformation_config.CLASS:self.data_transformation_config.LABEL},inplace =True)
+
+            return raw_data
+
+        except Exception as e:
+            raise CustomException(e,sys) from e
+
+    def concat_dataframe(self):
+
+        try:
+            # Let's concatinate both the data into a single data frame.
+            frame = [self.raw_data_cleaning(), self.imbalance_data_cleaning()]
+            df = pd.concat(frame)
+            print(df.head())
+
+            return df
+
+        except Exception as e:
+            raise CustomException(e, sys) from e
+
+    def concat_data_cleaning(self, words):
+
+        try:
+            # Let's apply stemming and stopwords on the data
+            stemmer = nltk.SnowballStemmer("english")
+            stopword = set(stopwords.words('english'))
+            words = str(words).lower()
+            words = re.sub('\[.*?\]', '', words)
+            words = re.sub('https?://\S+|www\.\S+', '', words)
+            words = re.sub('<.*?>+', '', words)
+            words = re.sub('[%s]' % re.escape(string.punctuation), '', words)
+            words = re.sub('\n', '', words)
+            words = re.sub('\w*\d\w*', '', words)
+            words = [word for word in words.split(' ') if words not in stopword]
+            words=" ".join(words)
+            words = [stemmer.stem(words) for word in words.split(' ')]
+            words=" ".join(words)
+
+            return words 
+
+        except Exception as e:
+            raise CustomException(e, sys) from e
+
+    def initiate_data_transformation(self) -> DataTransformationArtifacts:
+        try:
+            logging.info("Entered the initiate_data_transformation method of Data transformation class")
+            self.imbalance_data_cleaning()
+            self.raw_data_cleaning()
+            df = self.concat_dataframe()
+            df[self.data_transformation_config.TWEET]=df[self.data_transformation_config.TWEET].apply(self.concat_data_cleaning)
+
+            os.makedirs(self.data_transformation_config.DATA_TRANSFORMATION_ARTIFACTS_DIR, exist_ok=True)
+            df.to_csv(self.data_transformation_config.TRANSFORMED_FILE_PATH)
+            # x = df[self.data_transformation_config.TWEET]
+            # y = df[self.data_transformation_config.LABEL]
+            # x_train,x_test,y_train,y_test = train_test_split(x,y, random_state = 42)
+            data_transformation_artifact = DataTransformationArtifacts(
+                transformed_data_path = self.data_transformation_config.TRANSFORMED_FILE_PATH
+            )
+
+            return data_transformation_artifact
+
+        except Exception as e:
+            raise CustomException(e, sys) from e
