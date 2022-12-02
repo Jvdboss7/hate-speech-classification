c4aa0351e85cc540bee350b9754aa11169f94d97
jaydeepIneuron007 <jaydeep@ineuron.ai>
2022-12-02 13:53:36 +0530

    data ingestion added

---
+++ demo.py
@@ -0,0 +1,4 @@
+from hate.pipeline.train_pipeline import TrainPipeline
+
+if __name__ == "__main__":
+    TrainPipeline().run_pipeline()
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="clothing-apparel-detection",
+    name="hate-speech-classification",
     version="0.0.1",
     author="iNeuron",
     author_email="cloud@ineuron.ai",
